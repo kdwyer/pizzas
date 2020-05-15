3ff4baabbd49e2ea4963c7887076dccc241835e5
Kev Dwyer <kevin.p.dwyer+PLUTOGIT@gmail.com>
2020-05-15 18:28:15 +0100

    Display basic pizza form

--- pizza/shop.py
+++ pizza/shop.py
@@ -1,11 +1,20 @@
 import bottle
+import wtforms
 
 
 app = bottle.Bottle()
 
 
 # FIXME deprecation warning about absolute template paths.
-@app.route("/")
+@app.route("/", name="index")
 @bottle.jinja2_view("index", template_lookup=["pizza/templates"])
 def index():
-    return {"title": "Pizza Shop"}
+    pizzas = [("margherita", 799), ("bianca", 899)]
+
+    class Form(wtforms.Form):
+        submit = wtforms.SubmitField()
+
+    for pizza, _ in pizzas:
+        setattr(Form, pizza, wtforms.BooleanField())
+    form = Form()
+    return {"app": app, "title": "Pizza Shop", "form": form, "pizzas": pizzas}
--- tests/view_tests/index_test.py
+++ tests/view_tests/index_test.py
@@ -10,3 +10,29 @@ def test_title():
     resp = app.get("/")
     html = resp.html
     assert html.title.string == "Pizza Shop"
+
+
+def test_form_is_post():
+    resp = app.get("/")
+    form = resp.form
+    assert form.method == "POST"
+
+
+def test_form_action():
+    resp = app.get("/")
+    form = resp.form
+    assert form.action == "/"
+
+
+def test_form_has_submit_button():
+    resp = app.get("/")
+    form = resp.form
+    assert "submit" in form.fields
+
+
+def test_pizzas_are_displayed():
+    resp = app.get("/")
+    form = resp.form
+    expected_names = frozenset(["margherita", "bianca"])
+    names = {v.name for v, in form.fields.values() if v.name != "submit"}
+    assert names >= expected_names
