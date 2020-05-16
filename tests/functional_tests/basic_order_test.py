c9db9aa5b6f950de28d2661ba6ce39a5904df3d6
Kev Dwyer <kevin.p.dwyer+PLUTOGIT@gmail.com>
2020-05-16 12:29:12 +0100

    Add functional test for basic order

---
+++ tests/functional_tests/basic_order_test.py
@@ -0,0 +1,31 @@
+import webtest
+
+import pizza
+
+
+app = webtest.TestApp(pizza.application)
+
+
+def test_can_place_order():
+    # 1. goto page
+    index = app.get("/")
+    # 2. pizzas and prices displayed
+    ihtml = index.html
+    labels = ihtml("label")
+    assert "Margherita" in [label.string for label in labels]
+    prices = ihtml.find_all("span", class_="item-price")
+    assert "7.99" in [price.string for price in prices]
+    # 2.1 select pizza
+    form = index.forms[0]
+    form["margherita"] = True
+    # 3. submitted
+    resp = form.submit()
+    # 4. redirect to order conf
+    assert resp.status == 302
+    # 5. order & price displayed
+    rhtml = resp.html
+    items = rhtml.findall("span.line-item")
+    assert len(items) == 1
+    assert items[0].string == "margherita"
+    prices = rhtml.find_all("span", class_="line-item-price")
+    assert prices[0].string == "7.99"
